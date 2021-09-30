78dcf241ed7f2c80d83769e0b1bcf0913f445cdf
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 03:23:15 +0530

    [Vaibhav] added ability for greeting app to greeting message with firstname and lastname

--- main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -3,6 +3,7 @@ package com.bridgelabz.greetingapp.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,18 @@ import com.bridgelabz.greetingapp.model.Greeting;
 
 @RestController
 public class GreetingController {
-
-	private static final String template = "Hello, %s";
+	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam (value="name",defaultValue="world") String name) {
 		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	}
+	
+	@RequestMapping(value = {"/query"},method = RequestMethod.GET)
+	public Greeting greeting(@RequestParam (value="name",defaultValue="") String name,@RequestParam (value="firstName",defaultValue="") String firstName,@RequestParam (value="lastName",defaultValue="") String lastName) {
+		 
+		return new Greeting(counter.incrementAndGet(),"Hello "+ name + firstName + " " + lastName);
+		
 	}
 }
