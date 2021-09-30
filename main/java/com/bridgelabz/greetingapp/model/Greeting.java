84eaffb77bf732dc0f184446381651b3fac23b94
Vaibhav Kokate <vaibhav.kokate@morningstar.com>
2021-10-01 03:17:15 +0530

    [Vaibhav] added greeting controller for different HTTP method

---
+++ main/java/com/bridgelabz/greetingapp/model/Greeting.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.greetingapp.model;
+
+public class Greeting {
+	private long id;
+	private String message;
+	public Greeting(long id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+	public long getId() {
+		return this.id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return this.message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}	
+}
